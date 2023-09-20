3422213877effa5cdc3fbf3520762d4078a40e41
Guido Cardarelli <guidocarda@hotmail.com>
2023-09-19 23:42:31 -0300

    update criminals

--- server/controllers/criminals.controllers.js
+++ server/controllers/criminals.controllers.js
@@ -1,11 +1,14 @@
 import { pool } from "../db.js";
 
 export const getCriminals = async (req, res) => {
-  const [rows] = await pool.query(
-    `SELECT * FROM criminals ORDER BY created_at DESC`
-  );
-  console.log(rows);
-  res.json(rows);
+  try {
+    const [rows] = await pool.query(
+      `SELECT * FROM criminals ORDER BY created_at DESC`
+    );
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const getCriminal = async (req, res) => {
@@ -19,49 +22,63 @@ export const getCriminal = async (req, res) => {
     if (results.length === 0) {
       return res.status(400).json({ message: "Criminal not found" });
     }
-    console.log(results);
-    return res.json(results);
-  } catch (e) {
-    console.log(e);
-  }
 
-  res.json("getting a criminal");
+    return res.json(results);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const createCriminal = async (req, res) => {
   const { name, nickname } = req.body;
-  console.log(name, nickname);
+
   try {
     const [results] = await pool.query(
       `INSERT INTO criminals (name, nickname) VALUES (?,?)`,
       [name, nickname]
     );
 
-    res.json({
+    return res.json({
       id: results.insertId,
       name,
       nickname,
     });
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-  res.send("creating a criminal");
 };
 
-export const updateCriminal = (req, res) => {
-  res.send("updating a criminal");
+export const updateCriminal = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [results] = await pool.query("UPDATE criminals SET ? WHERE id = ?", [
+      req.body,
+      id,
+    ]);
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json("Criminal not found");
+    }
+
+    res.json({ id, ...req.body });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deleteCriminal = async (req, res) => {
   const { id } = req.params;
+  try {
+    const [results] = await pool.query("DELETE FROM criminals WHERE id = ?", [
+      id,
+    ]);
 
-  const [results] = await pool.query("DELETE FROM criminals WHERE id = ?", [
-    id,
-  ]);
+    if (results.affectedRows === 0) {
+      return res.status(404).json("Criminal not found");
+    }
 
-  if (results.affectedRows === 0) {
-    return res.status(404).json("Criminal not found");
+    res.status(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-
-  res.status(204);
 };
